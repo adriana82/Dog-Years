ac6f7858e98e8990ceca93d6ec2a43a549de0463
adriana82 <32023795+adriana82@users.noreply.github.com>
2017-10-03 16:35:40 -0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+let myAge= 35;
+// this is the first varible
+let earlyYears= 2;
+//the value of this variable will change
+earlyYears*= 10.5;
+laterYears= myAge- 2;
+//these are the first two years
+laterYears*= 4;
+//these are the next four years
+myAgeInDogYears= earlyYears+ laterYears;
+let myName="Adriana";
+"Adriana".toLowerCase();
+console.log(`My name is ${myName}. I am ${myAgeInDogYears} years old in dog years.`);
